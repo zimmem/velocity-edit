2cf9c8e5d113197cdfb35b2303e9e53705e3219e
sefler.zy@gmail.com <sefler.zy@gmail.com@a0fb69ed-743d-de5c-e050-684b070ee9b1>
2012-08-02 04:12:30 +0000

    improved word completion with cluster phenomena considered
    git-svn-id: http://velocity-edit.googlecode.com/svn/trunk@23 a0fb69ed-743d-de5c-e050-684b070ee9b1

--- src/com/hudson/velocityweb/editors/velocity/completion/WordCompleteEngine.java
+++ src/com/hudson/velocityweb/editors/velocity/completion/WordCompleteEngine.java
@@ -3,7 +3,6 @@ package com.hudson.velocityweb.editors.velocity.completion;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
